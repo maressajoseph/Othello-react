93b213cb444aad21465a90a360b078ad1f961d97
maressajoseph <maressa.joseph@gmail.com>
2017-06-21 12:45:50 +0200

    Trying to render game

--- othello-react/src/games/Game.js
+++ othello-react/src/games/Game.js
@@ -7,7 +7,10 @@ export class Game extends PureComponent {
   static propTypes = {
     _id: PropTypes.string.isRequired,
     board: PropTypes.array.isRequired,
-    players: PropTypes.array.isRequired,
+    players: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
     started: PropTypes.bool.isRequired,
     winner: PropTypes.number,
     draw: PropTypes.bool,
@@ -19,10 +22,24 @@ export class Game extends PureComponent {
   }
 
 
-
   render() {
-    return
+    const {
+      id,
+      board,
+      players,
+      winner,
+      draw,
+      turn
+    } = this.props
+
+    return(
+      <div>
+        <h3>{ players }</h3>
+      </div>
+    )
   }
 }
 
-export default Game
+const mapStateToProps = ({ games }) => ({ games })
+
+export default connect(mapStateToProps)(Game)
